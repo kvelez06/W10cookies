3f0447c5f29317e3770b8f0ac23ceb33d1d5b9a5
Karl <heroware.06@hotmail.com>
2020-08-27 23:19:00 -0600

    added cookies and working

--- app.js
+++ app.js
@@ -13,6 +13,18 @@
 //     counterText.innerHTML = counter;
 // }
 
-function sun() {
-    Cookies.set("synthCookie", "sun", {expires:1})
+
+function SetCar1() {
+    Cookies.set("selection", "Honda");
+    window.open("selection.html");
+}
+
+function SetCar2() {
+    Cookies.set("selection", "Nissan");
+    window.open("selection.html");
+}
+
+function SetCar3() {
+    Cookies.set("selection", "Toyota");
+    window.open("selection.html");
 }
